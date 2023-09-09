ad36595a2a7c44d9dc7cbfec5bcd354111b4af9d
Arev <vardanyanarevik23@gmail.com>
2023-09-09 18:03:02 +0400

    variable values geting and seting

--- closure.js
+++ closure.js
@@ -13,3 +13,31 @@ const createCounter = function(){
 const counter = createCounter();
 // console.log(counter()); // Output: 1
 // console.log(counter());
+
+
+// !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+// 2
+const privateVariable = function(num){
+    let numVal = num
+        const get = function(){
+            return numVal
+        }
+        const set = function(value){
+            numVal = value
+            return numVal
+        }
+
+        return {
+            get,
+            set
+        }
+    
+}
+
+
+// const secretValue = privateVariable(42); 
+// console.log(secretValue.get()); // Output: 42 
+// secretValue.set(1005); 
+// console.log(secretValue.get());
+
+
