dfe7badaa8767105275498a4cbc6c72cb92a59a9
Arev <vardanyanarevik23@gmail.com>
2023-09-11 10:41:43 +0400

    ex-7 createCalculator giving as an argument operation name

--- closure.js
+++ closure.js
@@ -1,16 +1,23 @@
-// !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-// 5
+// !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+// 7
 
+function createCalculator(str){
 
-function add(a = 0, b = 0, c = 0) { 
-    return a + b + c; 
-} 
-function partial(foo,num){
-    return function(a,b){
-        return foo(num,a,b) 
+    switch(str){
+        case "add":
+            return function(a,b){
+                return a+b
+        };
+        case "multiply":
+            return function(a,b){
+                return  a * b
+        }
     }
+        
+    
 }
-const add5 = partial(add,5);
-console.log(add5(10,20))
+
+const multiply = createCalculator("add"); 
+// console.log(multiply(2,8))
 
 
