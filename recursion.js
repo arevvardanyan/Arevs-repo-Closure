48668b5b1e128802c94a45214622142e53344e5f
Arev <vardanyanarevik23@gmail.com>
2023-09-11 10:48:26 +0400

    ex-1 factorial using recursion

---
+++ recursion.js
@@ -0,0 +1,14 @@
+// !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+// 1
+
+const factorial = function(a){
+    if(a === 0){
+        return 1
+    }
+
+    return  a *  factorial(a-1)
+}
+
+// console.log(factorial(5))
+
+
