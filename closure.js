ec990c819226d86cd8d8270f77b8b2e8a47cfcdc
Arev <vardanyanarevik23@gmail.com>
2023-09-11 10:36:17 +0400

    ex-3 caching with object

--- closure.js
+++ closure.js
@@ -1,74 +1,32 @@
-// !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-// 1
-const createCounter = function(){
-    let i = 0;
-
-    return function(){
-        i++
-        return i
-    }
-}
-
-
-const counter = createCounter();
-// console.log(counter()); // Output: 1
-// console.log(counter());
-
-
-// !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-// 2
-const privateVariable = function(num){
-    let numVal = num
-        const get = function(){
-            return numVal
-        }
-        const set = function(value){
-            numVal = value
-            return numVal
-        }
-
-        return {
-            get,
-            set
-        }
-    
-}
-
-
-// const secretValue = privateVariable(42); 
-// console.log(secretValue.get()); // Output: 42 
-// secretValue.set(1005); 
-// console.log(secretValue.get());
-
-
 // !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 3
+
 const createCachingFunction = function(){
-    let cache  = [];
-    console.log(cache)
+    let obj = {}
     return function(num){
-
-        let multi = num * 2;
-
-            if(cache.length === 0){
-                cache.push(num)
-                return `${multi} (calculated)`
+        let multi = num * 2
+        if(obj.length === 0){
+            obj[num] = num
+            return `${multi} (calculated)`
+        }else{
+            if(num in obj){
+                return `${multi} (cached)`
             }else{
-                for(let i = 0; i< cache.length; i++){
-                    if(!(cache.includes(num))){
-                        cache.push(num)
-                        return `${multi} (calculated)`
-                    }else{
-                        cache.push(num)
-                        return `${multi} (cached)`
-                    }
-            
-    
-                }
+                
+                obj[num] = num
+                return `${multi} (calculated)`
             }
-         
+            
+        }
+
     }
-     
 }
+
+
+const cachedCalculation = createCachingFunction(); 
+// console.log(cachedCalculation(5)); // Output: 10 (calculated)
+// console.log(cachedCalculation(6));
+// console.log(cachedCalculation(5));
+
 
 
