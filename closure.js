e60eb8f589ac83aacb42e231cdab313b0cbd5ec9
Arev <vardanyanarevik23@gmail.com>
2023-09-11 10:43:59 +0400

    ex-8 closure function with different callings

--- closure.js
+++ closure.js
@@ -1,23 +1,32 @@
-// !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-// 7
+// !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+// 8
 
-function createCalculator(str){
 
-    switch(str){
-        case "add":
-            return function(a,b){
-                return a+b
-        };
-        case "multiply":
-            return function(a,b){
-                return  a * b
+function add(a , b , c ) { 
+    return a + b + c; 
+} 
+
+const curry = function(foo){
+    return function(...a){
+        let sum1;
+        if(a.length === 3){
+            return foo(...a)
+        }else{
+            sum1 = [...a]
         }
-    }
+       return function(...b){
+
+        if(sum1.length === 2){
+            return sum1[0] + sum1[1] + b[0]
+        }
         
-    
+            return function(...c){
+                return foo(a[0],b[0],c[0])
+            }
+       }
+    } 
 }
-
-const multiply = createCalculator("add"); 
-// console.log(multiply(2,8))
-
-
+const curriedAdd = curry(add); 
+// console.log(curriedAdd(1)(2)(3));
+// console.log(curriedAdd(1,2)(3));
+// console.log(curriedAdd(1,2,3));
